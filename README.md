66dbf57ae202280009e2da41e1e1d20f195cb5f2
Brian Bates <124414764+brianbates928@users.noreply.github.com>
2024-02-28 08:44:53 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-I am Brian Bates and this is my Personal Website! Type https://brianjbates.xyz in any browser of your choice to access! It has both a mobile and laptop version for optimal viewing experience. Thank you!
+I am Brian Bates and this is my Personal Website!
+Type https://brianjbates.xyz in any browser of your choice to access!
+It has both a mobile and laptop version for optimal viewing experience.
+Thank you!
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import { styles } from "./HomeStyles";
+
+const Home = () => {
+  const MobileBeach = `${process.env.PUBLIC_URL}/images/Forest.jpg`;
+
+  const isMobile = window.innerWidth < 900;
+
+  return (
+    <div style={styles.background}>
+      {isMobile && (
+        <img style={styles.image} src={MobileBeach} alt="MobileBeach" />
+      )}
+      <div style={styles.info}>
+        <h style={styles.name}>Brian Bates</h>
+        <h style={styles.title}>Software Developer!</h>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
